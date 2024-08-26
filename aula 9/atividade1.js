9b1c16c6345e0075b10926789d23da1d56c5ae5c
vih_faller <vitoriafallerbier@gmail.com>
2024-08-26 14:59:09 -0300

    aula9

---
+++ aula 9/atividade1.js
@@ -0,0 +1,25 @@
+/*
+let array 
+console.log('a.  ', array) // Imprime Undefined
+
+array = null
+console.log('b.  ', array) // Imprime valor Null
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log('c.  ', array.length) // Imprime 11
+
+let i = 0
+console.log('d.  ', array[i]) // Imprime 3
+
+array[i+1] = 19
+console.log('e.  ', array) // Imprime [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+const valor = array[i+6]
+console.log('f.  ', valor) // Imprime 9
+*/
+
+/*
+const frase = prompt('Digite uma frase')
+console.log(frase.toUpperCase().replaceAll('A', 'I'), frase.length) 
+// Se a frase for "Socorram-me subi no ônibus em Marrocos", será impresso "SOCORRIM-ME SUBI NO ÔNIBUS EM MIRROCOS 38" 
+*/
