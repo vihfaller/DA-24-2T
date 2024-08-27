4894cdcc45db9851cae02d3ed52b9411f6858596
vih_faller <vitoriafallerbier@gmail.com>
2024-08-27 17:22:20 -0300

    aula 10

---
+++ aula 10/ativ1.js
@@ -0,0 +1,181 @@
+//------if -> se
+//------else -> se nao
+
+/*
+let condicao1 = true
+if(condicao1){
+    //açao
+    console.log ("exibiu condicao1")
+}
+let condicao2 = false 
+if(condicao2){
+    //açao
+    console.log ("exibiu condicao2")
+}
+*/
+
+/*
+const condiçao1 = 1 === 2 
+console.log (condiçao1)
+const condiça2 = 1 === "2" 
+console.log (condiçao2)
+--> O resultado foi false.
+*/
+
+/*
+let condiçao1 = 1
+let condiçao2 = 2
+if(condiçao1===condiçao2){
+    console.log ("os numeros sao iguais")
+}
+--> descobrindo se os valores sao iguais ou diferentes.
+*/ 
+
+/*
+let condiçao = false
+if(condiçao){
+    console.log("a condiçao é verdadeira")
+}else{
+    console.log("a condiçao é falsa")
+}
+*/
+
+/*
+let num1=20
+let num2=30
+if (num1===num2){
+    console.log ("os numeros sao iguais")
+}else{
+    console.log ("os numeros sao diferente")
+}
+*/
+
+/*
+let condiçao1= false
+let condiçao2= false
+if (condiçao1){
+    console.log("condiçao1 verdadeiro")
+}else if (condiçao2){
+        console.log ("condiçao2 é verdadeira")
+}else{
+    console.log("entrou no else")
+}
+*/
+
+/*
+let primeiroValor=10
+let segundoValor=20
+if (primeiroValor===segundoValor){
+    console.log("os numeros sao iguais")
+}else if (primeiroValor>segundoValor){
+    console.log ("o primeiro valor é maior que o segundo")
+}else {
+    console.log ("o primeiro é menor que o segundo numero")
+}
+*/
+
+/*
+let paísDeOrigem = prompt ("digite seu país de origem:")
+switch(paisDeOrigem){
+    case 'Brasil':
+        console.log ("brasileiro")
+        break
+    case "EUA":
+        console.log ("norte americano")
+        break
+    case "Inglaterra":
+        console.log ("ingles")
+        break
+    default :
+        console.log("nenhuma nacionalidade foi encontrada")
+    break
+}
+*/
+
+/*
+let pokemon = prompt ("escolha um pokemon e escreva o nome dele abaixo e descubra qual sua habilidade:")
+switch (pokemon){
+    case 'Bulbasaur':
+        console.log("planta e veneno")
+        break
+    case 'Charmander':
+        console.log ("fogo")
+        break
+    case 'Squirtle':
+        console.log ("água")
+        break
+    case 'Dugtrio':
+        console.log ("terra")
+        break
+    case 'Meowth':
+        console.log ("normal")
+        break 
+    case 'Persian':
+        console.log("normal")
+        break
+    case 'Psyduck':
+        console.log ("água")
+        break
+    case 'Golduck':
+        console.log ("água")
+        break
+    case 'Mankey':
+        console.log ("lutador")
+        break
+    default :
+        console.log ("voce nao soube escrever certo ou nao esta na lista")
+}
+*/
+
+/*
+let harryPotter = prompt ("diga um nome de um personagem e descubra a casa dele:")
+switch(harryPotter){
+    case 'Hermione':
+    console.log ("Grifnória")
+    break
+    case 'Ana':
+        console.log ("Lufa-Lufa")
+        break
+    case 'Luna':
+        console.log ("Corvinal")
+        break
+    case 'Nascisa':
+        console.log ('Sonserina')
+    default :
+    console.log ("Nao achamos")
+}
+*/
+
+/*
+let cond1 = true
+let cond2= false
+if (cond1&&cond2){ -----------> seria "e"
+    console.log ("as duas condiçoes sao verdadeiras")
+}
+else{
+    console.log ("alguma condiçao é falsa")
+}
+---------------------
+let cond1 = true
+let cond2= false
+if (cond1||cond2){ ----------->
+    console.log ("algumas das condiçoes sao verdadeiras")
+}
+else{
+    console.log ("todas sao falsas")
+}
+if (!cond1){ -----------> diferença
+    console.log ("a condiçao 1 é falsa")
+}else{
+    console.log("a condiçao é verdadeira")
+}
+*/
+
+
+let ensinoMedio = prompt ("Já concluiu o ensino médio?")
+console.log (ensinoMedio)
+let idade = prompt ("Possui 18 anos ou mais?")
+console.log (idade)
+let faculdade = prompt ("Estuda em alguma instituiçao de ensino superior?")
+console.log (facul)
+
